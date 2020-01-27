c5c22f6b453cc9b422a764f8ee298db944c60d3e
Jack Smith <jsmithtech2019@gmail.com>
2020-01-26 19:27:39 -0600

    Dummy data kept reinserting into db

--- lib/utils/home_widget.dart
+++ lib/utils/home_widget.dart
@@ -41,38 +41,6 @@ class _HomeState extends State<Home> {
   ];
   @override
   Widget build(BuildContext context) {
-    // TODO: remove test data insertion lines
-    // Insert test data for debugging development
-    // Get singleton
-    var dbHelper = GetIt.instance<DatabaseHelper>();
-
-    // Dummy Truck Test Data
-    dbHelper.executeRawQuery('INSERT INTO TRUCK_TEST_DATA '
-        '(test1_result, test1_current, '
-        'test2_result, test2_current, '
-        'test3_result, test3_current, '
-        'test4_result, test4_current) '
-        'VALUES '
-        '(0, 21.0, '
-        '1, 1.24, '
-        '0, 13.9, '
-        '1, .98)');
-    // Dummy Trailer Test Data
-    dbHelper.executeRawQuery('INSERT INTO TRAILER_TEST_DATA '
-        '(test1_result, test1_current, '
-        'test2_result, test2_current, '
-        'test3_result, test3_current, '
-        'test4_result, test4_current) '
-        'VALUES '
-        '(0, 21.0, '
-        '1, 1.24, '
-        '0, 13.9, '
-        '1, .98)');
-
-    // Dummy User Data
-    dbHelper.executeRawQuery('INSERT INTO CUSTOMER_DATA (name, phone, email, addr1, addr2, city, state, zip, truckplate, trailerplate) VALUES ('
-        '"Jack Smith", "11111", "dummy@gmail.com", "addr1", "addr2", "cstat", "tx", "7777", "truckplateval", "trailerplateval")');
-    
     return Scaffold(
       body: _children[_currentIndex],
       backgroundColor: Colors.black12,
